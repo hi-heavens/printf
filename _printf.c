09931ac9207cfdcb1d18fc8bc00972f9fa33e539
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-07 16:37:51 +0100

    An updated _printf file

--- _printf.c
+++ _printf.c
@@ -1,19 +1,27 @@
 #include <stdarg.h>
-
+#include "main.h"
+/**
+* _printf - Entry point
+* Description - A function that produces output
+* *@format: the function accepts an input saved into
+* format (Character string)
+* Return: Total count of the printed character
+*/
 int _printf(const char *format, ...)
 {
-        char *myString;
+        int printed_chars;
+	print_t format_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
 
         va_list arg;
+
+        if (format == NULL)
+                return (-1);
+
         va_start(arg, format);
 
-        for(myString = format; *myString != '\0'; myString++)
-        {
-                while(*myString != '%')
-                {
-                        putchar(*myString);
-                        myString++;
-                }
-                myString++;
-        }
 }

c3a8aee97d72c08d496d7ea56f91f54d59c347d7
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-07 16:06:49 +0100

    A function that writes the input to stdout

---
+++ _write_char.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _write_char - Entry point
+* Description - A function that writes the input to stdout
+* *@c: the function accepts an input saved into n
+* Return: Success (0)
+*/
+int _write_char(char c)
+{
+        return (write(1, &c, 1));
+}

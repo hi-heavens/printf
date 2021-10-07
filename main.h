01d3d0831e4c302597d2243fdc1dee17286e83d2
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-07 17:37:01 +0100

    Updated header file

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
  * @t: type to print
  * @f: function to print type
  */
-typedef struct print
+struct print
 {
   char *t;
   int (*f)(va_list);
@@ -20,5 +20,9 @@ typedef struct print print_t;
 int _putchar(char c);
 int _write(char);
 int _printf(const char *format, ...);
+int format_count(const char *format, print_t format_list[], va_list arg);
+int _print_char(va_list);
+int _print_string(va_list);
+int _print_percent(va_list);
 
 #endif  /* _MAIN_H */

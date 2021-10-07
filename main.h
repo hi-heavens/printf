2d1fa53ca510a3d1e37ab0613c91f7ae90f36d5a
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-07 16:37:33 +0100

    An updated header file

--- main.h
+++ main.h
@@ -1,19 +1,24 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 /**
  * struct print - structure for printing various types
  * @t: type to print
- * @f: function to print
+ * @f: function to print type
  */
 typedef struct print
 {
   char *t;
   int (*f)(va_list);
-} print_t;
+};
+
+typedef struct print print_t;
 
 int _putchar(char c);
+int _write_char(char);
 int _printf(const char *format, ...);
 
 #endif  /* _MAIN_H */

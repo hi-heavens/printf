744c69cf341300849dbc111d04789f9fe4de4c9e
Kehinde Adedokun <ken_adex@yahoo.com>
2021-10-09 04:54:59 +0100

    An updated file

--- main.h
+++ main.h
@@ -26,7 +26,6 @@ int print_string(va_list);
 int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
-int print_binary(va_list);
 int print_reversed(va_list arg);
 int rot13(va_list);
 int unsigned_integer(va_list);
